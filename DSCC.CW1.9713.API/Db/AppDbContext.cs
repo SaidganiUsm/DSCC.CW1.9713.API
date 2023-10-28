14c2d595d07539dd9f8f246f3f92b6ebddbd16db
SaidganiUsm <saidgani110@gmail.com>
2023-10-28 13:00:06 +0500

    [3] DbContext for creating migration to my database

---
+++ DSCC.CW1.9713.API/Db/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DSCC.CW1._9713.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSCC.CW1._9713.API.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define the primary key for the Customer entity
+            modelBuilder.Entity<Customer>()
+                .HasKey(c => c.Id);
+
+            // Define the primary key for the Order entity
+            modelBuilder.Entity<Order>()
+                .HasKey(o => o.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
