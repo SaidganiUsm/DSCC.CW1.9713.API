76ac7d5a50096a9be28ea1a10027f41c8761e8c7
SaidganiUsm <saidgani110@gmail.com>
2023-10-28 12:59:37 +0500

    [2] Models for the future API

---
+++ DSCC.CW1.9713.API/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DSCC.CW1._9713.API.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
