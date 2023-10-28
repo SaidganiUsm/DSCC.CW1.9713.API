da3e6d4c538ba86f7b78ae5016f6481124c29195
SaidganiUsm <saidgani110@gmail.com>
2023-10-28 12:59:23 +0500

    [1] Setting up the project

---
+++ DSCC.CW1.9713.API/Program.cs
@@ -0,0 +1,38 @@
+using DSCC.CW1._9713.API.Db;
+using DSCC.CW1._9713.API.Models;
+using DSCC.CW1._9713.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IService<Customer>, CustomerService>();
+builder.Services.AddScoped<IService<Order>, OrderService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
