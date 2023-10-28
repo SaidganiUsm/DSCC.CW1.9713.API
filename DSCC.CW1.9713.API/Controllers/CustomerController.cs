feba14e06af301a35dd87c000f571a50fa45139b
SaidganiUsm <saidgani110@gmail.com>
2023-10-28 13:01:10 +0500

    [6] Controllers for 2 entities

---
+++ DSCC.CW1.9713.API/Controllers/CustomerController.cs
@@ -0,0 +1,86 @@
+﻿using DSCC.CW1._9713.API.Models;
+using DSCC.CW1._9713.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSCC.CW1._9713.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IService<Customer> _customerService;
+
+        public CustomerController(IService<Customer> customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            return Ok(_customerService.GetAll());
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var customer = _customerService.GetById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _customerService.Delete(id);
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer(Customer customer) 
+        {
+            if (ModelState.IsValid)
+            {
+                _customerService.Create(customer);
+                return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+            }
+
+            return Ok(customer);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCustomer(int id, Customer updatingCustomer) 
+        {
+            if (id != updatingCustomer.Id)
+            {
+                return BadRequest("User is not exists in database");
+            }
+
+            var existingOrder = _customerService.GetById(id);
+
+            if (existingOrder == null)
+            {
+                return NotFound();
+            }
+
+            // Update the properties of the existing order with the new values.
+            existingOrder.Name = updatingCustomer.Name;
+            existingOrder.Email = updatingCustomer.Email;
+            existingOrder.City = updatingCustomer.City;
+            existingOrder.Country = updatingCustomer.Country;
+            existingOrder.Phone = updatingCustomer.Phone;
+
+            _customerService.Update(existingOrder);
+            return Ok(existingOrder);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCustomer(int id)
+        {
+            var customer = _customerService.GetById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+    }
+}
---
+++ DSCC.CW1.9713.API/Controllers/OrderController.cs
@@ -0,0 +1,84 @@
+﻿using DSCC.CW1._9713.API.Models;
+using DSCC.CW1._9713.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSCC.CW1._9713.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IService<Order> _orderService;
+
+        public OrderController(IService<Order> orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            var orders = _orderService.GetAll();
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrder(int id)
+        {
+            var order = _orderService.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _orderService.Create(order);
+                return CreatedAtAction(nameof(GetOrder), new { id = order.Id }, order);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateOrder(int id, Order updatedOrder)
+        {
+            if (id != updatedOrder.Id)
+            {
+                return BadRequest("Invalid request");
+            }
+
+            var existingOrder = _orderService.GetById(id);
+
+            if (existingOrder == null)
+            {
+                return NotFound();
+            }
+
+            // Update the properties of the existing order with the new values.
+            existingOrder.Amount = updatedOrder.Amount;
+            existingOrder.Name = updatedOrder.Name;
+            existingOrder.TotalPrice = updatedOrder.TotalPrice;
+
+            _orderService.Update(existingOrder);
+            return Ok(existingOrder);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            var order = _orderService.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _orderService.Delete(id);
+            return NoContent();
+        }
+    }
+}
